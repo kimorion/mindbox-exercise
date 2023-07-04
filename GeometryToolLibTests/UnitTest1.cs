94ed528a670d1882c0b3c4be7e4caee7abe7b816
kbannyi <kbannyi@custis.ru>
2023-07-04 18:07:39 +0300

    feat: Init
    Refs: mindbox-exercise

---
+++ GeometryToolLibTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace GeometryToolLibTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
